b9723d3bea269a73eae410f7ec693c0a7c096245
SolarACM <monarrattanabrsm@gmail.com>
2026-05-15 16:34:16 +0700

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // สั่งให้ Vercel ไม่ต้องเช็ก Lint ตอน Build ค่ะ
     ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // ป้องกัน Error อื่นๆ ที่อาจจะตามมาด้วยค่ะ
-    ignoreBuildErrors: true,
   },
 };
 
